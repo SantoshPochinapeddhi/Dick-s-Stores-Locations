e3a70ac5c77ec53a74333c76bf21a20222cfc5e6
SantoshPochinapeddhi <santoshpochinapeddhi@gmail.com>
2017-12-22 09:50:41 -0800

    Initial project setup

---
+++ app/src/main/java/com/sample/dicksstores/LocationListActivity.java
@@ -0,0 +1,13 @@
+package com.sample.dicksstores;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocationListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_list);
+    }
+}
